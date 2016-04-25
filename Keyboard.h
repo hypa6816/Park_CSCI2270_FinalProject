df9f11a801d5486e0a18876abba072d4967fa892
Sara <hypa6816@colorado.edu>
2016-04-25 17:26:38 -0600

    add Header

---
+++ Keyboard.h
@@ -0,0 +1,44 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Key{
+	char letter;
+	Key *next;
+    Key *previous;
+
+	Key(){};
+	Key(char in_letter)
+	{
+		letter = in_letter;
+		next = NULL;
+		previous = NULL;
+	}
+
+};
+
+class Keyboard
+{
+	public:
+		Keyboard();
+		~Keyboard();
+		void createKeyboard(int row, int column);
+		void printKeyboard();
+		void wordTraversing(std::string word);
+		int *findKeyIndex(string letter);
+		void deleteKey(std::string name);
+		void insertKey(std::string name);
+		int Row;
+		int Column;
+	private:
+
+		int keyboardSize;
+		Key* keyboard[100];
+
+};
+
+#endif // KEYBOARD_H
